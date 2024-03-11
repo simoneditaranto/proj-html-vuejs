2f495e62f7080390cb78b3ba0cf47baef6951065
s.ditaranto <simodita99@gmail.com>
2024-03-11 12:27:48 +0100

    macro-header completato

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,18 @@
 <script>
+import AppNav from './AppNav.vue';
 
 export default {
     name: 'AppHeader',
+
+    components: {
+        AppNav,
+    },
+
+    data() {
+        return {
+
+        }
+    }
 }
 
 </script>
@@ -10,24 +21,73 @@ export default {
 
     <header>
 
-        <nav></nav>
+        <AppNav></AppNav>
+
+        <div id="jumbo">
+
+            <h3>Imagine, Create, Experience</h3>
+
+            <h1>We Animate With Ninja-Like Precision</h1>
+
+            <p>We are a character-driven animation studio striving to make high-quality animations.</p>
+
+            <div class="jumbo-buttons">
+                <div class="btn secondary">Start a Project</div>
+                <div class="btn primary">Portfolio</div>
+            </div>
+
+            <img src="/img/middle.png" alt="" class="video-intro">
+
+            <img src="/img/left-man.png" alt="" class="left-man">
+            <img src="/img/right-man.png" alt="" class="right-man">
+
+        </div>
 
     </header>
 
 </template>
 
 <style scoped lang="scss">
-
+@use '../styles/variables' as *;
 header{
-    height: 800px;
-    background-color: #311852;
-    /* test */
+    color: white;
 
-    nav{
-        width: 100%;
-        height: 70px;
-        border: 1px solid black;
-        /* test */
+    background-image: url('/img/banner-bg.png');
+    background-size: cover;
+    h1{
+        font-size: 3em;
+    }
+
+    h3{
+        /*color: $primaryColor;*/
+        color: #ff7a1f;
+    }
+
+    #jumbo{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 30px;
+
+        position: relative;
+
+        .jumbo-buttons{
+            display: flex;
+            gap: 24px;
+        }
+
+        .video-intro{
+            transform: translateY(50%);
+        }
+
+        .left-man{
+            position: absolute;
+            left: 0;
+        }
+        .right-man{
+            position: absolute;
+            right: 0;
+        }
     }
 }
 
---
+++ src/components/AppNav.vue
@@ -0,0 +1,62 @@
+<script>
+
+export default {
+    name: 'AppNav',
+
+    props: {
+        linkName: String,
+    }
+}
+
+</script>
+
+<template>
+
+    <nav>
+        <div id="nav-container">
+        
+            <img src="/img/logo.png" alt="logo">
+
+            <ul>
+                <li>Testo</li>
+                <li>Testo</li>
+                <li>Testo</li>
+                <li>Testo</li>
+                <li>Testo</li>
+                <li>Testo</li>
+                <li>Testo</li>
+                <li class="btn secondary">Get a Quote</li>
+            </ul>
+
+        </div>
+    </nav>
+
+</template>
+
+<style scoped lang="scss">
+nav{
+    padding: 20px 0;
+    margin-bottom: 85px;
+
+    color: white;
+
+    #nav-container{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        margin: 0 auto;
+
+        width: 80%;
+
+        ul{
+            display: flex;
+            align-items: center;
+            gap: 20px;
+
+            font-weight: bold;
+        }
+    }
+}
+
+</style>
