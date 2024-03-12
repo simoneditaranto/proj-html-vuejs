f09f4e642585d91be7633157695089e6a7ed55c7
s.ditaranto <simodita99@gmail.com>
2024-03-12 13:00:02 +0100

    sezione "create" completata (macro)

--- src/components/AppCreate.vue
+++ src/components/AppCreate.vue
@@ -10,18 +10,82 @@ export default {
 
     <section id="create">
 
-        
+        <div class="titles">
+
+            <h3>
+                We Create New Worlds!
+            </h3>
+
+            <h2>
+                Want Tips On Your Storytelling?
+            </h2>
+
+            <p>
+                Join our newsletter for tips on how to elevate your brand througt storytelling
+            </p>
+
+        </div>
+
+        <div class="register">
+         <input type="text" placeholder="Enter your email"> 
+            <div class="btn secondary">Submit</div>
+        </div>
 
     </section>
 
 </template>
 
 <style scoped lang="scss">
+@use '../styles/variables' as *;
 
 #create{
-    height: 300px;
-    background-color: #311852;
-    /* test */
+
+    padding: 85px 0;
+
+    background-image: url('/img/preloader.png');
+    background-size: cover;
+
+    .titles {
+        
+        h3{
+            color: $fourthColor;
+        }
+
+        h2{
+            color: white;
+        }
+
+        p{
+            color: white;
+        }
+
+    }
+
+    .register{
+        position: relative;
+        
+        margin: 0 auto;
+        
+        width: 70%;
+
+        input{
+            padding: 26px 10px;
+    
+            width: 100%;
+    
+            border-radius: 40px;
+            border: none;
+
+            background-color: white;
+        }
+
+        .btn{
+            position: absolute;
+            right: 10px;
+            top: 10%;
+        }
+    }
 }
+
 
 </style>
