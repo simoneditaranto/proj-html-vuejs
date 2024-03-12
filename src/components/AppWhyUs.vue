10a808e87dd415137b99b3e155266af3511ff64a
s.ditaranto <simodita99@gmail.com>
2024-03-12 11:58:03 +0100

    sezione "why-us" completata (macro)

--- src/components/AppWhyUs.vue
+++ src/components/AppWhyUs.vue
@@ -10,7 +10,23 @@ export default {
 
     <section id="why-us">
 
-        
+        <div class="titles">
+
+            <h3>Why Anidio studio?</h3>
+
+            <h2>
+                We Love Our Clients
+            </h2>
+
+            <p>
+                We are trusted throughout adland and have a wealth of reccomendatlons from start-ups to renowed global brands
+            </p>
+
+            <div class="container brands">
+                <img src="/img/sponsor5.png" alt="" v-for="n in 5" class="brand">
+            </div>
+
+        </div>
 
     </section>
 
@@ -19,10 +35,17 @@ export default {
 <style scoped lang="scss">
 
 #why-us{
-    height: 300px;
-    border: 3px dotted black;
-    background-color: rgba(0, 0, 0, 0.4);
-    /* test */
+    padding-top: 135px;
+
+    .brands{
+        display: flex;
+        flex-wrap: wrap;
+        gap: 20px;
+
+        .brand {
+            width: calc(100% / 5 - 20px / 5 * 4);
+        }
+    }
 }
 
 </style>
